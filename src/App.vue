91479a6e69aad13bdad1bd54f219f95d7d371596
Peter Poliwoda <p.poliwoda@zoovu.com>
2024-03-14 13:59:49 +0100

    Create Vue Wordle task

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div class="row">Hello Wordle!</div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  justify-content: center;
+}
+
+.cell {
+  width: 60px;
+  height: 60px;
+  border: 1px solid black;
+  margin: 5px;
+  font-size: 50px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.backgroundGrey {
+  background: #d4d4d4;
+}
+
+.backgroundGreen {
+  background: #b3faa8;
+}
+
+.backgroundYellow {
+  background: #ffe788;
+}
+</style>
